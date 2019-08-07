9a71329f6c465b82f5ca78a1c26838d69dfb86e5
mvb <mvb@teknologisk.dk>
2019-08-07 16:06:16 +0200

    init

---
+++ src/rqt_pattern_manager/pattern_manager.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Danish Technological Institute (DTI)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Mads Vainoe Baatrup
+
+import os
+import rospy
+import rospkg
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
+
+
+class PatternManagerPlugin(Plugin):
+    def __init__(self, context):
+        super(PatternManagerPlugin, self).__init__(context)
+        self.setObjectName('PatternManagerPlugin')
+
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        parser.add_argument("-q", "--quiet", action="store_true", dest="quiet", help="Put plugin in silent mode")
+        
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
+
+        self._widget = QWidget()
+
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_pattern_manager'), 'resource', 'PatternManagerPlugin.ui')
+        loadUi(ui_file, self._widget)
+
+        self._widget.setObjectName('PatternManagerPluginUi')
+
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+
+        context.add_widget(self._widget)
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
